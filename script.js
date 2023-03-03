e1dfb73e155f338fbc52f14837a6b7a9c4912616
lomaferreira <lomasantos666@gmail.com>
2023-03-03 19:49:33 -0300

    add js

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-function changes(){
+const form=document.getElementById('form')
+//const click = document.getElementById('button');//
+form.addEventListener('submit', changes);
+const resp = document.getElementById('resp');
+
+function changes(event) { 
+event.preventDefault(
+ const input = document.getElementById('email').value;
+ if (input == "") {
+    //resp.InnerText="Please provide a valid email";
+    alert('Please provide a valid email');
+ return false; 
+}
+) 
     
+ 
+ 
+ 
 }
