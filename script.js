03c752943e8a258d6bba5c62c67ecf3234c83b5f
lomaferreira <lomasantos666@gmail.com>
2023-03-17 16:43:53 -0300

    validate email

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 const form = document.querySelector('form');
-const input = document.querySelector('input[name="email"]');
-const btn = document.querySelector('#button');
+const input = document.querySelector('#email');
+const Emailregex =
+  /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
+  //escrever no html
+const resp = document.querySelector('.resp-erro');
+var textValid = document.createTextNode('Valid Email Address');
+var textInvalid = document.createTextNode('Please provide a valid email ');
+;
 const validateInput = () => {
-  if (!input.value) {
-    //INDICA PRA PEGAR O PROXIMO ELEMENTO//
-    btn.nextElementSibling.classList.remove('resp-erro');
-
+  if (Emailregex.test(input.value)) {
+    resp.appendChild(textValid);
+  } else {
+     resp.appendChild(textInvalid);
   }
 };
 
